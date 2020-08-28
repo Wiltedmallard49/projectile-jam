979840a6030325b392185c5ccc9e92dabb2781fe
Wiltedmallard49 <69858723+Wiltedmallard49@users.noreply.github.com>
2020-08-28 11:14:45 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,43 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . 5 . . . . . . . . 
-                    . . . . . . . 5 5 5 . . . . . . . 
-                    . . . . . . . 5 5 5 . . . . . . . 
-                    . . . . . . . 5 5 5 . . . . . . . 
-                    . . . . . . . 5 5 5 . . . . . . . 
-                    . . . . . . . 5 5 5 . . . . . . . 
-                    . . . . . . . 5 5 5 . . . . . . . 
-                    . . . . . . . 5 5 5 . . . . . . . 
-                    . . . . . . . 5 5 5 . . . . . . . 
-                    . . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . .
+        . . . . . . . . 5 . . . . . . . . 
+        . . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
         `, spacePlane, 0, speed)
+    music.pewPew.play()
 })
-sprites.onDestroyed(SpriteKind.Enemy, function on_on_destroyed(sprite: Sprite) {
-    
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
     Rock = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . b b b b b b . . . . . 
-                    . . . . b c c c c b b b b . . . 
-                    . . . c c b a c c a b a b b . . 
-                    . . . c c a b b b b a b b b . . 
-                    . . c c b b b a b b b a b b . . 
-                    . . c c a c c c a c c c a b . . 
-                    . . c c c b b c a a c c b b . . 
-                    . . c c a c c c c c c c b b . . 
-                    . . . c c a c c c a c c b . . . 
-                    . . . . a c c c a c c . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . b b b b b b . . . . . 
+        . . . . b c c c c b b b b . . . 
+        . . . c c b a c c a b a b b . . 
+        . . . c c a b b b b a b b b . . 
+        . . c c b b b a b b b a b b . . 
+        . . c c a c c c a c c c a b . . 
+        . . c c c b b c a a c c b b . . 
+        . . c c a c c c c c c c b b . . 
+        . . . c c a c c c a c c b . . . 
+        . . . . a c c c a c c . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
     Rock.setPosition(randint(10, 110), randint(10, 20))
     Rock.setVelocity(0, 50 + Difficulty)
@@ -46,85 +45,74 @@ sprites.onDestroyed(SpriteKind.Enemy, function on_on_destroyed(sprite: Sprite) {
         if (info.score() % 10 == 0) {
             Difficulty += 1
             if (info.life() < 5) {
+                music.powerUp.play()
                 info.changeLifeBy(1)
             }
-            
         }
-        
-        if (speed < -150) {
-            if (info.score() % 50 == 0) {
-                speed += -5
-            }
-            
+        if (info.score() % 50 == 0) {
+            speed += -10
         }
-        
     }
-    
-    if (info.score() == 150) {
-        game.over(true, effects.confetti)
-    }
-    
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
-    Rock.setPosition(randint(10, 110), 0)
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     Rock.destroy()
     projectile.destroy()
     info.changeScoreBy(1)
 })
-let projectile : Sprite = null
+let projectile: Sprite = null
 let speed = 0
 let Difficulty = 0
-let Rock : Sprite = null
-let spacePlane : Sprite = null
+let Rock: Sprite = null
+let spacePlane: Sprite = null
 effects.starField.startScreenEffect()
 spacePlane = sprites.create(img`
-        . . . . . . . 4 4 . . . . . . . 
-            . . . . . . 4 4 4 4 . . . . . . 
-            . . . . . 2 4 4 4 4 2 . . . . . 
-            . . . . 2 2 2 2 2 2 2 2 . . . . 
-            . . . 2 2 8 2 2 2 2 8 2 2 . . . 
-            . . 2 2 2 8 2 2 2 2 8 2 2 2 . . 
-            . . 2 2 2 8 2 2 2 2 8 2 2 2 . . 
-            . . 2 2 8 8 2 2 2 2 8 8 2 2 . . 
-            . . 2 2 2 8 2 2 2 2 8 2 2 2 . . 
-            . . 2 2 2 8 2 2 2 2 8 2 2 2 . . 
-            . . . 2 2 8 2 2 2 2 8 2 2 . . . 
-            . . . 2 2 8 2 2 2 2 8 2 2 . . . 
-            . . . 2 2 4 2 2 2 2 4 2 2 . . . 
-            . . . 2 2 4 2 2 2 2 4 2 2 . . . 
-            . . . 2 4 . 4 2 2 4 . 4 2 . . . 
-            . . . 2 . . . 2 2 . . . 2 . . .
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . 4 4 4 4 . . . . . . 
+    . . . . . 2 4 4 4 4 2 . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . 2 2 8 2 2 2 2 8 2 2 . . . 
+    . . 2 2 2 8 2 2 2 2 8 2 2 2 . . 
+    . . 2 2 2 8 2 2 2 2 8 2 2 2 . . 
+    . . 2 2 8 8 2 2 2 2 8 8 2 2 . . 
+    . . 2 2 2 8 2 2 2 2 8 2 2 2 . . 
+    . . 2 2 2 8 2 2 2 2 8 2 2 2 . . 
+    . . . 2 2 8 2 2 2 2 8 2 2 . . . 
+    . . . 2 2 8 2 2 2 2 8 2 2 . . . 
+    . . . 2 2 4 2 2 2 2 4 2 2 . . . 
+    . . . 2 2 4 2 2 2 2 4 2 2 . . . 
+    . . . 2 4 . 4 2 2 4 . 4 2 . . . 
+    . . . 2 . . . 2 2 . . . 2 . . . 
     `, SpriteKind.Player)
 spacePlane.setFlag(SpriteFlag.StayInScreen, true)
 spacePlane.setPosition(80, 100)
 controller.moveSprite(spacePlane, 150, 150)
 Rock = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . b b b b b b . . . . . 
-            . . . . b c c c c b b b b . . . 
-            . . . c c b a c c a b a b b . . 
-            . . . c c a b b b b a b b b . . 
-            . . c c b b b a b b b a b b . . 
-            . . c c a c c c a c c c a b . . 
-            . . c c c b b c a a c c b b . . 
-            . . c c a c c c c c c c b b . . 
-            . . . c c a c c c a c c b . . . 
-            . . . . a c c c a c c . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . b b b b b b . . . . . 
+    . . . . b c c c c b b b b . . . 
+    . . . c c b a c c a b a b b . . 
+    . . . c c a b b b b a b b b . . 
+    . . c c b b b a b b b a b b . . 
+    . . c c a c c c a c c c a b . . 
+    . . c c c b b c a a c c b b . . 
+    . . c c a c c c c c c c b b . . 
+    . . . c c a c c c a c c b . . . 
+    . . . . a c c c a c c . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Enemy)
 Rock.setVelocity(0, 50 + Difficulty)
 Rock.setPosition(randint(10, 110), randint(10, 20))
 Difficulty = 0
 speed = -100
 info.setLife(1)
-forever(function on_forever() {
+music.playMelody("E F E G E A E D ", 120)
+forever(function () {
     if (Rock.y >= 120) {
         Rock.destroy()
         info.changeLifeBy(-1)
     }
-    
 })
